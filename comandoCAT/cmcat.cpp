35214a433bbdaadd2a5467ee066279f00bd6e8ab
Hector <hr9457@gmail.com>
2021-03-14 22:39:16 -0600

    comando cat para la lectura de archivo --- ultimo commit

---
+++ comandoCAT/cmcat.cpp
@@ -0,0 +1,6 @@
+#include "comandocat.h"
+
+comandoCat::comandoCat()
+{
+
+}
---
+++ comandoCAT/cmcat.h
@@ -0,0 +1,11 @@
+#ifndef COMANDOCAT_H
+#define COMANDOCAT_H
+
+
+class comandoCat
+{
+public:
+    comandoCat();
+};
+
+#endif // COMANDOCAT_H
